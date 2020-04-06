2fc4c0c7ce4d7249505424957de00362cdd59d40
Markus Wind <markuswind@gmail.com>
2020-04-06 13:25:10 +0200

    feat: added input fields for name, birthday and introduction

---
+++ src/pages/Form/index.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+
+function Form() {
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <h1>Resume Form</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input
+          name="firstName"
+          defaultValue=""
+          ref={register({ required: true })}
+        />
+        {errors.firstName && "First name is required"}
+
+        <input
+          name="lastName"
+          defaultValue=""
+          ref={register({ required: true })}
+        />
+        {errors.lastName && "Last name is required"}
+
+        <input
+          type="date"
+          name="birthdate"
+          defaultValue=""
+          ref={register({ required: true })}
+        />
+        {errors.birthdate && "Birthdate is required"}
+
+        <input
+          name="introduction"
+          defaultValue=""
+          ref={register({ required: true })}
+        />
+        {errors.introduction && "Introduction is required"}
+
+        <input type="submit" />
+      </form>
+    </div>
+  );
+}
+
+export default Form;
