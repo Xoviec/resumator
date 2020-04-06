b61015c78df367c056625582dc8d0c6500d25a8a
Markus Wind <markuswind@gmail.com>
2020-04-06 13:33:41 +0200

    feat: use placeholder values as default values

--- src/pages/Form/index.js
+++ src/pages/Form/index.js
@@ -1,9 +1,18 @@
 import React from "react";
 import { useForm } from "react-hook-form";
 
+// TODO: remove when firebase values are truly fetched
+const firebaseValues = {
+  firstName: "John",
+  lastName: "Doe",
+  birthdate: "1980-01-30",
+  city: "Amsterdam",
+};
+
 function Form() {
   const { register, handleSubmit, errors } = useForm();
   const onSubmit = (data) => {
+    // TODO: save values in firebase
     console.log(data);
   };
 
@@ -14,25 +23,29 @@ function Form() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <input
           name="firstName"
-          defaultValue=""
+          defaultValue={firebaseValues.firstName}
           ref={register({ required: true })}
         />
         {errors.firstName && "First name is required"}
 
         <input
           name="lastName"
-          defaultValue=""
+          defaultValue={firebaseValues.lastName}
           ref={register({ required: true })}
         />
         {errors.lastName && "Last name is required"}
 
-        <input name="city" defaultValue="" ref={register({ required: true })} />
+        <input
+          name="city"
+          defaultValue={firebaseValues.city}
+          ref={register({ required: true })}
+        />
         {errors.city && "City is required"}
 
         <input
           type="date"
           name="birthdate"
-          defaultValue=""
+          defaultValue={firebaseValues.birthdate}
           ref={register({ required: true })}
         />
         {errors.birthdate && "Birthdate is required"}
