4db2b409b7f13c6225be5bf0dea1f7c0eeae53cd
Sebastiaan van Arkens <sebastiaan.van.arkens@frontmen.nl>
2020-04-07 11:53:25 +0200

    Merged conflict that I missed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react";
-<<<<<<< HEAD
-import { ThemeProvider } from "emotion-theming";
-import "./assets/css/global.css";
-import theme from "./config/theme";
-import { Button, Flex, Box, Heading, Text } from "rebass";
-import frontmenLogo from "./assets/svg/frontmen-logo.svg";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import HomePage from "./pages/Home";
+import PdfPreviewer from "./pages/PdfPreviewer";
+import LoginLayout from "./pages/layout/LoginLayout";
 import FirebaseAppContextProvider from "./context/FirebaseContext";
 import FirebaseTest from "./components/FirebaseTest";
 
@@ -27,51 +25,19 @@ const firebaseConfig = {
   messagingSenderId,
   appId,
 };
-
-const App = () => {
-  return (
-    <FirebaseAppContextProvider config={firebaseConfig}>
-      <ThemeProvider theme={theme}>
-        <FirebaseTest />
-        <Flex
-          flexDirection="column"
-          justifyContent="center"
-          alignItems="center"
-          bg="secondary"
-        >
-          <Box p="2rem">
-            <img src={frontmenLogo} alt="logo" />
-          </Box>
-          <Box width="100%" p="2rem" color="white" bg="white" textAlign="center">
-            <Heading fontSize={7} color="secondary" mb="3">
-              Frontmen Resumator
-            </Heading>
-            <Text fontSize={4} color="text" mb="4">
-              Welcome to the Frontmen Resumator, a tool to generate Resumes
-            </Text>
-            <Button variant="primary" p="1rem">
-              Login with Frontmen account
-            </Button>
-          </Box>
-        </Flex>
-      </ThemeProvider>
-    </FirebaseAppContextProvider>
-=======
-import { BrowserRouter, Switch, Route } from "react-router-dom";
-import HomePage from "./pages/Home";
-import PdfPreviewer from "./pages/PdfPreviewer";
-import LoginLayout from "./pages/layout/LoginLayout";
-
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Switch>
-          <Route exact path="/" component={HomePageWrapper} />
-          <Route exact path="/previewer" component={PdfPreviewerWrapper} />
-        </Switch>
-      </div>
-    </BrowserRouter>
+    <FirebaseAppContextProvider config={firebaseConfig}>
+      <BrowserRouter>
+        <div className="App">
+          <FirebaseTest />
+          <Switch>
+            <Route exact path="/" component={HomePageWrapper} />
+            <Route exact path="/previewer" component={PdfPreviewerWrapper} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    </FirebaseAppContextProvider>
   );
 }
 
@@ -87,7 +53,6 @@ const PdfPreviewerWrapper = () => {
     <LoginLayout>
       <PdfPreviewer />
     </LoginLayout>
->>>>>>> develop
   );
 };
 
