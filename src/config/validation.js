1b3fc6228fe401717ba4ba06bb02f928f45b6beb
Markus Wind <markuswind@gmail.com>
2020-04-10 11:17:55 +0200

    feat: added validation for experience fields

--- src/config/validation.js
+++ src/config/validation.js
@@ -3,7 +3,20 @@ import * as yup from "yup";
 export default yup.object().shape({
   firstName: yup.string().required(),
   lastName: yup.string().required(),
-  dateOfBirth: yup.date(),
+  dateOfBirth: yup.date().required(),
   city: yup.string().required(),
   introduction: yup.string().required().max(200),
+  experience: yup
+    .array()
+    .min(1)
+    .of(
+      yup.object().shape({
+        company: yup.string().required(),
+        role: yup.string().required(),
+        from: yup.date().required(),
+        untill: yup.date().required(),
+        description: yup.string().required().max(200),
+        stackAndTechniques: yup.array().min(1),
+      })
+    ),
 });
