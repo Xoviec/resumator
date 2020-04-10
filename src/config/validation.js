8e1a2efa2b02b525c42eb53c908054f7ee59c6be
Markus Wind <markuswind@gmail.com>
2020-04-10 11:14:07 +0200

    fix: use date validation and set max chars on introduction

--- src/config/validation.js
+++ src/config/validation.js
@@ -3,7 +3,7 @@ import * as yup from "yup";
 export default yup.object().shape({
   firstName: yup.string().required(),
   lastName: yup.string().required(),
-  dateOfBirth: yup.string().required(),
+  dateOfBirth: yup.date(),
   city: yup.string().required(),
-  introduction: yup.string().required(),
+  introduction: yup.string().required().max(200),
 });
