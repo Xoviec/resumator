66e8db28bebbbf4cdd83ecd1c7f32587fd93f869
Markus Wind <markuswind@gmail.com>
2020-04-20 13:12:56 +0200

    feat: added avatarinput component

--- src/components/CreatorComponents/index.js
+++ src/components/CreatorComponents/index.js
@@ -1,2 +1,3 @@
+export { default as AvatarInput } from "./AvatarInput";
 export { default as EducationInput } from "./EducationInput";
 export { default as ExperienceInput } from "./ExperienceInput";
