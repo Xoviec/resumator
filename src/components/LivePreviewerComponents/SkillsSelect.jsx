f856633d13cd0dd1e6da73c0d3e983d7a96359fd
Rafael <rfviolato@gmail.com>
2020-06-15 15:40:41 +0200

    feat: add ability to re-order skills via drag and drop
    86

--- src/components/LivePreviewerComponents/SkillsSelect.jsx
+++ src/components/LivePreviewerComponents/SkillsSelect.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { TextField, InputLabel } from "@material-ui/core";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import { TextField } from "@material-ui/core";
+import styled from "@emotion/styled";
 import { Autocomplete } from "@material-ui/lab";
 import Chip from "@material-ui/core/Chip";
 import { skillsConstants } from "../../config/skills.constants";
@@ -10,25 +12,10 @@ const AUTOCOMPLETE_REASONS = {
   REMOVE: "remove-option",
 };
 
+// TODO: Deprecate label prop
 const SkillsSelect = ({ value, onChange, label }) => {
-  const renderSelectedSkills = (currentSkills, getTagProps) =>
-    currentSkills.map(({ name }, index) => (
-      <Chip
-        key={name}
-        label={name}
-        size="small"
-        variant="outlined"
-        color="secondary"
-        {...getTagProps({ index })}
-      />
-    ));
-
   const renderInput = (params) => (
-    <TextField
-      {...params}
-      variant="outlined"
-      placeholder="Add frameworks, libraries, technologies..."
-    />
+    <TextField {...params} placeholder="Add a library, framework, skill..." />
   );
 
   const onAutocompleteChange = (event, inputValue, reason) => {
@@ -56,26 +43,87 @@ const SkillsSelect = ({ value, onChange, label }) => {
 
   const getOptionSelected = (option, skill) => option === skill.name;
 
+  const reorderSkills = (from, to) => {
+    const skillList = [...value];
+
+    // Swap position
+    skillList.splice(to, 0, skillList.splice(from, 1)[0]);
+
+    onChange(skillList);
+  };
+
   return (
-    <>
-      {label && <InputLabel id="skill-label">{label}</InputLabel>}
+    <DragDropContext
+      onDragEnd={({ source, destination }) =>
+        reorderSkills(source.index, destination.index)
+      }
+    >
+      <Droppable droppableId="droppable" direction="horizontal">
+        {(provided) => (
+          <SkillsWrapper ref={provided.innerRef} {...provided.droppableProps}>
+            {value.map(({ name }, index) => (
+              <Draggable key={name} draggableId={name} index={index}>
+                {({ draggableProps, dragHandleProps, innerRef }, { isDragging }) => (
+                  <CustomChip
+                    label={name}
+                    size="small"
+                    variant="outlined"
+                    color="secondary"
+                    ref={innerRef}
+                    style={draggableProps.style}
+                    isDragging={isDragging}
+                    onDelete={() => {}}
+                    {...draggableProps}
+                    {...dragHandleProps}
+                  />
+                )}
+              </Draggable>
+            ))}
+          </SkillsWrapper>
+        )}
+      </Droppable>
 
       <Autocomplete
         id="skill-list-autocomplete"
         multiple
         freeSolo
-        fullWidth
         disableClearable
         disableCloseOnSelect
-        options={skillsConstants}
+        fullWidth
         value={value}
-        renderTags={renderSelectedSkills}
+        options={skillsConstants}
         renderInput={renderInput}
         onChange={onAutocompleteChange}
         getOptionSelected={getOptionSelected}
+        renderTags={() => null}
       />
-    </>
+    </DragDropContext>
   );
 };
+
+const CustomChip = styled(Chip)`
+  margin-right: 8px;
+  background-color: #fff;
+`;
+
+const SkillsWrapper = styled.div`
+  display: flex;
+  height: 50px;
+  flex: 1 1 auto;
+  overflow: auto;
+  align-items: center;
+  border-radius: 4px;
+  border: 1px solid #c4c4c4;
+  padding: 0 8px;
+  margin-bottom: 8px;
+
+  &::after {
+    content: "";
+    display: block;
+    width: 30px;
+    height: 100%;
+    flex: 1 0 auto;
+  }
+`;
 
 export default SkillsSelect;
