006e3afd68055e90b719f30306f5b9ae0cc31fac
Jurgen Beliën <info@jurgenbelien.nl>
2020-08-04 15:07:58 +0200

    refactor(functions/types): split Resume type into chunks

--- functions/src/types/index.ts
+++ functions/src/types/index.ts
@@ -1,44 +1,55 @@
-export interface Resume {
-  personalia: {
+export interface Personalia {
     firstName: string;
     lastName: string;
     email: string;
     dateOfBirth: Date;
     city: string;
-  };
-  education: {
-    id?: string;
-    certificate?: boolean;
-    institute?: string;
-    name?: string;
-    startDate?: string;
-    endDate?: string;
-  }[];
+}
+export interface Education {
+  id?: string;
+  certificate?: boolean;
+  institute?: string;
+  name?: string;
+  startDate?: string;
+  endDate?: string;
+}
+export interface Experience {
+  id: string;
+  role: string;
+  company: string;
+  stackAndTechniques: [{ name: string }];
+  startDate: string;
+  endDate: string;
+}
+export interface Project {
+  id: string;
+  role: string;
+  company: string;
+  stackAndTechniques: [{ name: string }];
+  startDate: string;
+  endDate: string;
+}
+export interface SideProject { 
+  id: string; 
+  link: string; 
+  description: string 
+}
+export interface Publication { 
+  id: string; 
+  link: string; 
+  description: string 
+}
+
+export interface Resume {
+  personalia: Personalia
+  education: Education[];
   introduction: string;
   avatar: string;
   skills: { name: string }[];
-  sideProjects: { id: string; link: string; description: string }[];
-  publications: [{ id: string; link: string; description: string }];
-  experience: [
-    {
-      id: string;
-      role: string;
-      company: string;
-      stackAndTechniques: [{ name: string }];
-      startDate: string;
-      endDate: string;
-    }
-  ];
-  projects: [
-    {
-      id: string;
-      role: string;
-      company: string;
-      stackAndTechniques: [{ name: string }];
-      startDate: string;
-      endDate: string;
-    }
-  ];
+  sideProjects: SideProject[];
+  publications: Publication[];
+  experience: Experience[];
+  projects: Project[];
 }
 
 export type Opts = {
