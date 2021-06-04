c72dd6ffa51d9cf86583c5ad828d9dd221e788b0
Ye Jun Wu <yejunwu168@gmail.com>
2021-06-04 14:43:28 +0200

    test: ensure correct name & email gets displayed

--- src/components/LivePreviewerComponents/__tests__/TopSection.spec.tsx
+++ src/components/LivePreviewerComponents/__tests__/TopSection.spec.tsx
@@ -2,11 +2,39 @@ import React from 'react'
 import { render, screen } from '@testing-library/react';
 import {TopSection, PersonaliaModel} from '../TopSection';
 
-test('Expect name to be Jane Doe', () => {
-  const personalia = {} as PersonaliaModel
-  const onSubmit = () => null
+const defaultProps = {
+  personalia: {} as PersonaliaModel,
+  onSubmit: jest.fn()
+}
+
+test('expect name to be Jane Doe', () => {
   const fallbackName = 'Jane Doe'
   
-  render(<TopSection onSubmit={onSubmit} personalia={personalia} />)
+  render(<TopSection {...defaultProps} />)
   expect(screen.getByRole('heading', {level: 3})).toHaveTextContent(fallbackName)
+})
+
+test('expect fallback introduction to be displayed', () => {
+  const fallbackText = `Jane has nothing to tell you.`
+  
+  render(<TopSection {...defaultProps} />)
+  expect(screen.getByText(fallbackText)).toBeInTheDocument()
+})
+
+const personalia = {
+  firstName: 'Donald',
+  lastName: 'Trump',
+  email: 'donald.trump@frontmen.nl'
+} as PersonaliaModel
+
+test('expect name to be Donald Trump', () => {
+  const name = `${personalia.firstName} ${personalia.lastName}`
+
+  render(<TopSection personalia={personalia} onSubmit={defaultProps.onSubmit} />)
+  expect(screen.getByRole('heading', {level: 3})).toHaveTextContent(name)
+})
+
+test('expect email to be displayed', () => {
+  render(<TopSection personalia={personalia} onSubmit={defaultProps.onSubmit} />)
+  expect(screen.getByText(personalia.email)).toBeInTheDocument()
 })
