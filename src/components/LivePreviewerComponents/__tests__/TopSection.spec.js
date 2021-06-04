551c88c7c522869067d5e0d365c5db651be77981
Ye Jun Wu <yejunwu168@gmail.com>
2021-06-04 09:41:49 +0200

    test: ensure correct fallback name gets displayed

---
+++ src/components/LivePreviewerComponents/__tests__/TopSection.spec.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react';
+import {TopSection} from '../TopSection';
+
+test('Expect name to be Jane Doe', () => {
+  const personalia = {}
+  const fallbackName = 'Jane Doe'
+  render(<TopSection personalia={personalia} />)
+  expect(screen.getByRole('heading', {level: 3})).toHaveTextContent(fallbackName)
+})
