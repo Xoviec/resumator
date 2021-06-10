41df1f589f9037c12cea905d278a91567366382b
Ye Jun Wu <yejunwu168@gmail.com>
2021-06-10 11:30:19 +0200

    chore: use firebase test env for testing with cypress

--- src/context/FirebaseContext/getFirebaseConfig.js
+++ src/context/FirebaseContext/getFirebaseConfig.js
@@ -12,14 +12,25 @@ export default function () {
     });
   } else {
     return new Promise((resolve) => {
-      resolve({
-        apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-        authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-        databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-        projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-        storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-      });
+      if (process.env.REACT_APP_ENV === "cypress") {
+        resolve({
+          apiKey: process.env.REACT_APP_FIREBASE_TEST_API_KEY,
+          authDomain: process.env.REACT_APP_FIREBASE_TEST_AUTH_DOMAIN,
+          databaseURL: process.env.REACT_APP_FIREBASE_TEST_DATABASE_URL,
+          projectId: process.env.REACT_APP_FIREBASE_TEST_PROJECT_ID,
+          storageBucket: process.env.REACT_APP_FIREBASE_TEST_STORAGE_BUCKET,
+          messagingSenderId: process.env.REACT_APP_FIREBASE_TEST_MESSAGING_SENDER_ID,
+        });
+      } else {
+        resolve({
+          apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+          authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+          projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+          storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+          messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+        });
+      }
     });
   }
 }
