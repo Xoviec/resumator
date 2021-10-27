d89efa9b0a908bf899de0f1a45f6bfa778bbaf28
armanrose <arman.sarkisov.96@gmail.com>
2021-10-27 14:12:02 +0200

    feat(321): multiselect items styling changes

--- src/components/Material/truncatedChip.tsx
+++ src/components/Material/truncatedChip.tsx
@@ -1,13 +1,9 @@
 import { Chip, Tooltip } from "@mui/material";
 
-const truncateLabel = (content: string) => {
-  const maxLength = 12;
-  if (content.length < maxLength) return content;
-  return `${content.substr(0, maxLength)}...`;
-};
+// helpers
+import { truncateLabel } from "../../helpers";
 
-export const TruncateChip = (props: any) => {
-  const { label, limit, ...rest } = props;
+export const TruncateChip = ({ label, limit, ...rest }: any) => {
   return (
     <Tooltip title={label}>
       <Chip {...rest} label={truncateLabel(label)} />
