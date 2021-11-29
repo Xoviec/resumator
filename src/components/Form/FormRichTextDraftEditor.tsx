d9f9678b603253eff08702265d62b593228b73bb
Zukzuk <dave.timmerman@gmail.com>
2021-11-29 16:36:46 +0100

    fix(lint): trying other linting solution

--- src/components/Form/FormRichTextDraftEditor.tsx
+++ src/components/Form/FormRichTextDraftEditor.tsx
@@ -29,160 +29,159 @@ interface FormRichTextDraftEditorProps {
   onChange: (value: string) => void;
 }
 
-export const FormRichTextDraftEditor: VoidFunctionComponent<FormRichTextDraftEditorProps> =
-  ({ value, onChange }) => {
-    let content;
+const getFormRichTextDraftEditor: VoidFunctionComponent<FormRichTextDraftEditorProps> = ({ value, onChange }) => {
+  let content;
 
-    // Setup the editor.
-    try {
-      content = convertFromRaw(JSON.parse(value));
-    } catch (err) {
-      // If parsing fails, treat it as a raw text string.
-      const { contentBlocks, entityMap } = convertFromHTML(value);
-      content = ContentState.createFromBlockArray(contentBlocks, entityMap);
-    }
+  // Setup the editor.
+  try {
+    content = convertFromRaw(JSON.parse(value));
+  } catch (err) {
+    // If parsing fails, treat it as a raw text string.
+    const { contentBlocks, entityMap } = convertFromHTML(value);
+    content = ContentState.createFromBlockArray(contentBlocks, entityMap);
+  }
 
-    const editorRef = useRef<Editor>(null);
-    const [editorState, setEditorState] = useState(
-      EditorState.createWithContent(content)
-    );
+  const editorRef = useRef<Editor>(null);
+  const [editorState, setEditorState] = useState(
+    EditorState.createWithContent(content)
+  );
 
-    const handleFocus = (event: MouseEvent) => {
-      event.preventDefault();
-      event.stopPropagation();
-      editorRef?.current?.focus();
-    };
+  const handleFocus = (event: MouseEvent) => {
+    event.preventDefault();
+    event.stopPropagation();
+    editorRef?.current?.focus();
+  };
 
-    /**
-     * Save the changes to the form when focus is lost.
-     */
-    const handleBlur = () => {
-      const raw = convertToRaw(editorState.getCurrentContent());
-      onChange(JSON.stringify(raw));
-    };
+  /**
+   * Save the changes to the form when focus is lost.
+   */
+  const handleBlur = () => {
+    const raw = convertToRaw(editorState.getCurrentContent());
+    onChange(JSON.stringify(raw));
+  };
 
-    const renderBlockTypeButton = (
-      type: string,
-      tooltip: string,
-      icon: ReactNode
-    ) => (
-      <TooltipIconButton
-        tooltip={tooltip}
-        active={hasBlockType(type)}
-        onClick={() => toggleBlockType(type)}
-        onMouseDown={(event) => event.preventDefault()}
-      >
-        {icon}
-      </TooltipIconButton>
-    );
+  const renderBlockTypeButton = (
+    type: string,
+    tooltip: string,
+    icon: ReactNode
+  ) => (
+    <TooltipIconButton
+      tooltip={tooltip}
+      active={hasBlockType(type)}
+      onClick={() => toggleBlockType(type)}
+      onMouseDown={(event) => event.preventDefault()}
+    >
+      {icon}
+    </TooltipIconButton>
+  );
 
-    /**
-     * Set a block type on the current cursor location or selected text.
-     */
-    const toggleBlockType = (type: string) => {
-      setEditorState(RichUtils.toggleBlockType(editorState, type));
-    };
+  /**
+   * Set a block type on the current cursor location or selected text.
+   */
+  const toggleBlockType = (type: string) => {
+    setEditorState(RichUtils.toggleBlockType(editorState, type));
+  };
 
-    const hasBlockType = (type: string) => {
-      const selection = editorState.getSelection();
-      const block = editorState
-        .getCurrentContent()
-        .getBlockForKey(selection.getStartKey());
-      return block.getType() === type;
-    };
+  const hasBlockType = (type: string) => {
+    const selection = editorState.getSelection();
+    const block = editorState
+      .getCurrentContent()
+      .getBlockForKey(selection.getStartKey());
+    return block.getType() === type;
+  };
 
-    const renderStyleButton = (style: string, tooltip: string, icon: ReactNode) => (
-      <TooltipIconButton
-        tooltip={tooltip}
-        active={hasStyle(style)}
-        onClick={() => toggleStyle(style)}
-        onMouseDown={(event) => event.preventDefault()}
-      >
-        {icon}
-      </TooltipIconButton>
-    );
+  const renderStyleButton = (style: string, tooltip: string, icon: ReactNode) => (
+    <TooltipIconButton
+      tooltip={tooltip}
+      active={hasStyle(style)}
+      onClick={() => toggleStyle(style)}
+      onMouseDown={(event) => event.preventDefault()}
+    >
+      {icon}
+    </TooltipIconButton>
+  );
 
-    /**
-     * Set a style on the current cursor location or selected text.
-     */
-    const toggleStyle = (style: string) => {
-      setEditorState(RichUtils.toggleInlineStyle(editorState, style));
-    };
+  /**
+   * Set a style on the current cursor location or selected text.
+   */
+  const toggleStyle = (style: string) => {
+    setEditorState(RichUtils.toggleInlineStyle(editorState, style));
+  };
 
-    const hasStyle = (style: string) =>
-      editorState.getCurrentInlineStyle().has(style);
+  const hasStyle = (style: string) =>
+    editorState.getCurrentInlineStyle().has(style);
 
-    return (
+  return (
+    <Box
+      onClick={handleFocus}
+      onBlur={handleBlur}
+      sx={{
+        "& .DraftEditor-root": {
+          padding: "4.5px",
+          height: "unset",
+          overflow: "auto",
+        },
+        "& ul, ol": {
+          margin: 0,
+        },
+        display: "flex",
+        flexDirection: "column",
+        gridGap: 4,
+        paddingX: 1,
+        paddingY: 1,
+        height: 400,
+        flex: 1,
+      }}
+    >
+      {/* Styling options */}
       <Box
-        onClick={handleFocus}
-        onBlur={handleBlur}
         sx={{
-          "& .DraftEditor-root": {
-            padding: "4.5px",
-            height: "unset",
-            overflow: "auto",
-          },
-          "& ul, ol": {
-            margin: 0,
-          },
           display: "flex",
-          flexDirection: "column",
-          gridGap: 4,
-          paddingX: 1,
-          paddingY: 1,
-          height: 400,
-          flex: 1,
+          flexDirection: "row",
+          alignItems: "center",
         }}
       >
-        {/* Styling options */}
-        <Box
+        {/* Unordered list */}
+        {renderBlockTypeButton(
+          "unordered-list-item",
+          "Unordered List",
+          <FormatListBulletedIcon fontSize="small" />
+        )}
+        {/* Ordered list */}
+        {renderBlockTypeButton(
+          "ordered-list-item",
+          "Ordered List",
+          <FormatListNumberedIcon fontSize="small" />
+        )}
+        <Divider
+          orientation="vertical"
           sx={{
-            display: "flex",
-            flexDirection: "row",
-            alignItems: "center",
+            margin: "0 4px",
+            height: "80%",
           }}
-        >
-          {/* Unordered list */}
-          {renderBlockTypeButton(
-            "unordered-list-item",
-            "Unordered List",
-            <FormatListBulletedIcon fontSize="small" />
-          )}
-          {/* Ordered list */}
-          {renderBlockTypeButton(
-            "ordered-list-item",
-            "Ordered List",
-            <FormatListNumberedIcon fontSize="small" />
-          )}
-          <Divider
-            orientation="vertical"
-            sx={{
-              margin: "0 4px",
-              height: "80%",
-            }}
-          />
-          {/* Bold */}
-          {renderStyleButton("BOLD", "Bold", <FormatBoldIcon fontSize="small" />)}
-          {/* Italic */}
-          {renderStyleButton(
-            "ITALIC",
-            "Italic",
-            <FormatItalicIcon fontSize="small" />
-          )}
-          {/* Underlined */}
-          {renderStyleButton(
-            "UNDERLINE",
-            "Underlined",
-            <FormatUnderlinedIcon fontSize="small" />
-          )}
-        </Box>
-        <Divider />
-        <Editor
-          spellCheck
-          ref={editorRef}
-          editorState={editorState}
-          onChange={(state) => setEditorState(state)}
         />
+        {/* Bold */}
+        {renderStyleButton("BOLD", "Bold", <FormatBoldIcon fontSize="small" />)}
+        {/* Italic */}
+        {renderStyleButton(
+          "ITALIC",
+          "Italic",
+          <FormatItalicIcon fontSize="small" />
+        )}
+        {/* Underlined */}
+        {renderStyleButton(
+          "UNDERLINE",
+          "Underlined",
+          <FormatUnderlinedIcon fontSize="small" />
+        )}
       </Box>
-    );
-  };
+      <Divider />
+      <Editor
+        spellCheck
+        ref={editorRef}
+        editorState={editorState}
+        onChange={(state) => setEditorState(state)}
+      />
+    </Box>
+  );
+};
